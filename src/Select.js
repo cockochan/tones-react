c8b90002c2bc7cd3fda294541983663232481006
cockochan <56898295+cockochan@users.noreply.github.com>
2020-06-29 17:33:00 +0300

    three

--- src/Select.js
+++ src/Select.js
@@ -1,11 +1,15 @@
 import React from 'react';
 const Select =(props)=>{
-return <select onChange={props.selOct}>
+return (
+<div>
+  <span style={{fontWeight:'bolder', color:'red'}}>select octave  </span>
+<select onChange={props.selOct}>
   <option value="1">1</option>
   <option value="2" >2</option>
   <option value="3" >3</option>
   <option value="4">4</option>
   <option value="5">5</option>
 </select>
+</div>)
 }
 export default Select;
